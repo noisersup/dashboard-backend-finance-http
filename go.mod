d2380b0524d7d4302ef86dc7a6c29dd3d5fc7b9a
noisersup <patekiapl@gmail.com>
2021-04-26 23:04:58 +0200

    :sparkles: Added grpc client

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/noisersup/dashboard-backend-finance-http
 
 go 1.16
+
+require (
+	google.golang.org/grpc v1.37.0
+	google.golang.org/protobuf v1.25.0
+)
--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"log"
+
+	"github.com/noisersup/dashboard-backend-finance-http/pb"
+	"google.golang.org/grpc"
+)
+
 func main(){
+	conn,err := grpc.Dial("localhost:9000",grpc.WithInsecure()) //TODO: make grpc secure
+	if err != nil { log.Fatal(err) }
 	
+	defer func () {
+		if err = conn.Close(); err != nil{
+			log.Fatal(err)
+		}
+	}()
+	
+	client := pb.NewFinanceServiceClient(conn)
 }
