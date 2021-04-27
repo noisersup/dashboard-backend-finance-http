ba546886b83bbeeac1a03f320f427f5c76c91d97
noisersup <patekiapl@gmail.com>
2021-04-27 16:45:57 +0200

    :wrench: Added env variables support

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 
 	"github.com/gorilla/mux"
 
@@ -11,8 +14,20 @@ import (
 	"google.golang.org/grpc"
 )
 
+type ServiceConfig struct{
+	GRPCAddress	string
+	GRPCPort	int
+	
+	HttpPort	int
+}
+
 func main(){
-	conn,err := grpc.Dial("localhost:9000",grpc.WithInsecure()) //TODO: make grpc secure
+	config := getVars()
+
+	grpcPayload := fmt.Sprintf("%s:%d",config.GRPCAddress,config.GRPCPort)
+	log.Printf("Connecting to %s", grpcPayload)
+
+	conn,err := grpc.Dial(grpcPayload,grpc.WithInsecure()) //TODO: make grpc secure
 	if err != nil { log.Fatal(err) }
 	log.Print("gRPC Connection initialized")
 	defer func () {
@@ -22,14 +37,26 @@ func main(){
 	}()
 	
 	client := pb.NewFinanceServiceClient(conn)
-	
 	h := httphandlers.CreateHandlers(client)
-	
 	r := mux.NewRouter()
 
 	r.HandleFunc("/finances",h.GetGroups).Methods("GET")
 
-	log.Print("Initializing http server")
+	log.Printf("Initializing http server on :%d",config.HttpPort)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("%d",config.HttpPort),r))
+}
 
-	log.Fatal(http.ListenAndServe(":8000",r))
+
+func getVars() *ServiceConfig {
+	var config ServiceConfig
+
+	config.GRPCAddress = os.Getenv("GRPC_ADDRESS")
+
+	config.GRPCPort,_ = strconv.Atoi(os.Getenv("GRPC_PORT")) 
+	config.HttpPort,_ = strconv.Atoi(os.Getenv("HTTP_PORT")) 
+
+	if config.GRPCAddress=="" || config.GRPCPort==0 || config.HttpPort==0 { 
+		log.Fatal("ENV variables did not set")
+	}
+	return &config
 }
